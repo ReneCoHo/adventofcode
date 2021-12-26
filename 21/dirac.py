9bda8c2edd2b2d105636e797ca505d8734b039c4
René Heidenreich <heidenreich@gfai.de>
2021-12-26 19:24:23 +0100

    solve 21 2

--- 21/dirac.py
+++ 21/dirac.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python
-global d_count
-
 def roll(dice):
     return (dice % 100) + 1
 
@@ -34,9 +32,42 @@ def turn(P):
         if pts[1] >= 1000:
             return pts[0]*count
 
-        #print("pos:", P[0], P[1])
-        #print("pts:", pts[0], pts[1])
-        #print("dice", dice)
-
 P = [3, 5]
 print("res:", turn(P))
+
+
+win_map = {}
+
+def recur(player, positions, pts):
+    key = str(player) + ' ' + str(positions[0]) + ' ' + str(positions[1]) + ' ' + str(pts[0]) + ' ' + str(pts[1])
+
+    if key in win_map:
+        return win_map[key]
+
+    if pts[0] >= 21:
+        return [1, 0]
+    if pts[1] >= 21:
+        return [0, 1]
+
+    wins = [0, 0]
+
+    for i in range(1, 4):
+        for j in range(1, 4):
+            for k in range(1, 4):
+                next_pos = [positions[0], positions[1]]
+                next_pts = [pts[0], pts[1]]
+                next_pl = (player+1)%2
+
+                new_pos = (positions[player] + i + j + k - 1) % 10 + 1
+
+                next_pts[player] += new_pos
+                next_pos[player] = new_pos
+                
+                win_res = recur(next_pl, next_pos, next_pts)
+                wins[0] += win_res[0]
+                wins[1] += win_res[1]		
+
+    win_map[key] = wins
+    return wins
+
+print(max(recur(0, [3, 5], [0, 0])))
