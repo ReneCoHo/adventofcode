8c185c38c246b25e907da8e1a73507cf872e3eb7
René Heidenreich <heidenreich@gfai.de>
2021-12-27 14:22:39 +0100

    solve 22 2

--- 22/reactor.py
+++ 22/reactor.py
@@ -33,6 +33,10 @@ def areOverlap(a : tuple, b: tuple)->bool:
         a[1][1] >= b[1][0] and b[1][1]>=a[1][0] and \
         a[2][1] >= b[2][0] and b[2][1]>=a[2][0]
 
+def intersection(a:tuple, b:tuple)->tuple:
+    return ((max(a[0][0], b[0][0]), min(a[0][1], b[0][1])),\
+            (max(a[1][0], b[1][0]), min(a[1][1], b[1][1])),\
+            (max(a[2][0], b[2][0]), min(a[2][1], b[2][1])))
 
 def doInstructions(volume, instructions):
     n2 = len(volume) // 2
@@ -49,6 +53,32 @@ def doInstructions(volume, instructions):
                         volume[n2+iz][n2+iy][n2+ix] = 0
     return volume
 
+def  addInstruction(inst, add_cube, cubes ):
+    if inst:
+        sig = 1
+    else:
+        sig = -1
+
+    add_cubes = []
+    for c, sig in cubes:
+        if areOverlap(c, add_cube):
+            add_cubes.append((intersection(add_cube, c)  ,-sig))
+    if inst:
+        add_cubes.append((add_cube, 1))
+    return add_cubes
+    
+
+def Count(c:tuple):
+    return (c[0][1]-c[0][0]+1)*(c[1][1]-c[1][0]+1)*(c[2][1]-c[2][0]+1)
+
+def doListInstructions(instructions)->int:
+    cubes = []
+    for inst, x, y, z in instructions:
+        print(inst, x, y, z )
+        cubes += addInstruction(inst, (x,y,z), cubes )
+
+    return sum( Count(c)*s for c, s in cubes )
+
 def countOnes(volume):
     count = 0
     n = len(volume)
@@ -60,13 +90,15 @@ def countOnes(volume):
     return count
 
 path = os.path.dirname(__file__)
-file_path = os.path.join(path, "test3.txt")
+#file_path = os.path.join(path, "test3.txt")
 file_path = os.path.join(path, "input.txt")
 instructions, maxVal = readValues(file_path)
-print(maxVal)
+print("range", maxVal)
 
-maxVal = 50
-volume = np.zeros((2*maxVal+1,)*3)
-volume = doInstructions(volume, instructions)
-print(countOnes(volume))
+#maxVal = 50
+#volume = np.zeros((2*maxVal+1,)*3)
+#volume = doInstructions(volume, instructions)
+#print("sum", countOnes(volume))
 
+cube_sum = doListInstructions(instructions)
+print("sum", cube_sum)
