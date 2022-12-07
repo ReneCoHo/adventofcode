7e7bc396aeb4a0851427e7f76d3cf5a8576a9ac3
René Heidenreich <heidenreich@gfai.de>
2022-12-07 09:25:31 +0100

    try faster solution

--- 2022/6/Trouble.py
+++ 2022/6/Trouble.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python
 import os
+import time
 
 def findBegin(input, length):
     subCmp = {}
@@ -28,11 +29,29 @@ def findBegin(input, length):
             subset[i] = subset[i+1]
     return None
 
+# create a new set for every subset
+def find_start(data, header_len, start=0):
+    for i in range(start, len(data) - header_len):
+        if len(set(data[i:i + header_len])) == header_len:
+            return i + header_len
+
+# get the start time
+st = time.time()
+
 path = os.path.dirname(__file__)
 file_path = os.path.join(path, "input.txt")
 
 with open(file_path) as f:
     input = f.read()
 
-print(findBegin(input, 4))
-print(findBegin(input, 14))
+sop = find_start(input, 4)
+som = find_start(input, 14, sop)
+
+print(sop)
+print(som)
+#print(findBegin(input, 4))
+#print(findBegin(input, 14))
+
+# get the execution time
+elapsed_time = time.time() - st
+print('Execution time:', elapsed_time, 'seconds')
