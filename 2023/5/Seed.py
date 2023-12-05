705736cdb4a381e4a6782d02b5fe3eb4cbb261af
René Heidenreich <heidenreich@gfai.de>
2023-12-05 10:30:06 +0100

    day 5 part 2

--- 2023/5/Seed.py
+++ 2023/5/Seed.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python
 import os
+import numpy as np
+from collections import deque
+from operator import itemgetter
 
 path = os.path.dirname(__file__)
 file_path = os.path.join(path, "input.txt")
@@ -34,14 +37,78 @@ def product(maps, seed):
            
     return seed
 
+def overlap(a, b, c, d):
+	return not (a > d or b < c)
+
+def minRange(maps, seeds):
+    # start ranges
+    ss = [seeds[i:i+2] for i in range(0, len(seeds), 2)]
+    current = [(a, a + b - 1) for a, b in ss]
+    current = deque(current)
+
+    for step in maps:
+        new = deque()
+
+        while current:
+            a, b = current.popleft()
+
+            for dst, src, sz in step:
+                c = src
+                d = src + sz - 1
+                delta = dst - src
+
+                if not overlap(a, b, c, d):
+                    continue
+
+                # C---a---b---D completely inside
+                #     xxxxx
+                if c <= a <= d and c <= b <= d:
+                    new.append((a + delta, b + delta))
+                    break
+
+                # a---C---b---D escapes left
+                #     xxxxx
+                if c <= b <= d:
+                    # C---b (overlap)
+                    new.append((c + delta, b + delta))
+                    # a---C (no overlap)
+                    current.append((a, c - 1))
+                    break
+
+                # C---a---D---b escapes right
+                #     xxxxx
+                if c <= a <= d:
+                    # C---D (overlap)
+                    new.append((a + delta, d + delta))
+                    # D---b (no overlap)
+                    current.append((d + 1, b))
+                    break
+
+                # a---C---D---b escapes both sides
+                #     xxxxx
+                if a < c and b > d:
+                    # C---D (overlap)
+                    new.append((c + delta, d + delta))
+                    # a---C (no overlap)
+                    # D---b (no overlap)
+                    current.append((d + 1, b))
+                    current.append((a, c - 1))
+                    break
+            else:
+                # no overlap with any segment
+                new.append((a, b))
+
+        current = new
+    x = map(itemgetter(0), current)    
+    return min(x)
+
 with open(file_path) as f:
     input = f.read()
 
 maps, seeds = readInput(input)
 
-lowest = min([product(maps, s) for s in seeds])
+lowest1 = min([product(maps, s) for s in seeds])
+print("1:", lowest1)
 
-print("1:", lowest)
-
-
-#print("2 sum:", np.array(cards).sum())
+lowest2 = minRange(maps, seeds)
+print("2:", lowest2)
