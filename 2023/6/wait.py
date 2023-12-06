14792563d7e91cbda380fe6d302571a5b8ea6b2a
René Heidenreich <heidenreich@gfai.de>
2023-12-06 12:50:34 +0100

    speed up part 2

--- 2023/6/wait.py
+++ 2023/6/wait.py
@@ -1,4 +1,14 @@
 #!/usr/bin/python
+import math
+
+def calcWinF(times, distances):
+    win_prod = 1
+    for t, d in zip(times, distances):
+        if t%2 == 0:
+            win_prod *= 1 + 2*math.floor(math.sqrt( (t/2)**2 - d))
+        else:
+            win_prod *= 2*math.floor(0.5 + math.sqrt( (t/2)**2 - d))
+    return win_prod
 
 def calcWin(times, distances):
     win_prod = 1
@@ -17,7 +27,8 @@ distances = [295, 1734, 1278, 1210]
 #times = [7, 15, 30]
 #distances = [9, 40, 200]
 
-print("1:", calcWin(times, distances))
+#print("1:", calcWin(times, distances))# correct ans 1413720
+print("1:", calcWinF(times, distances))
 
 #times = [71530]
 #distances = [940200]
@@ -25,4 +36,4 @@ print("1:", calcWin(times, distances))
 times = [45988373]
 distances = [295173412781210]
 
-print("2:", calcWin(times, distances))
+print("2:", calcWinF(times, distances)) # correct ans 30565288
