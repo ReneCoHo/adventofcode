7ced23d3fe614a9b13238531d85bf8640044b10d
René Heidenreich <heidenreich@gfai.de>
2023-12-11 16:03:54 +0100

    day 9 part 2

--- 2023/9/Mirage.py
+++ 2023/9/Mirage.py
@@ -14,13 +14,24 @@ def extrapol(values):
         diff.append(values[i] - values[i-1])
     return diff[-1] + extrapol(diff)
 
+def extrapolL(values):
+    if all(v == 0 for v in values):
+        return 0
+    
+    diff = []
+    for i in range(1, len(values)):
+        diff.append(values[i] - values[i-1])
+    return diff[0] - extrapolL(diff)
+
 with open(file_path) as f:
     lines = f.read().splitlines()
 
 sum = 0
+sumL = 0
 for line in lines:   
     int_list = [int(i) for i in line.split(" ")]
     sum += int_list[-1] + extrapol(int_list)
-
+    sumL += int_list[0] - extrapolL(int_list)
 
 print("1 sum:", sum)
+print("2 sum:", sumL)
